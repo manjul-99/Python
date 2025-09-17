a7d7e9eaf44999eb902f4b719df4894285a4f3b1
manjul-99 <manjulrathod16@gmail.com>
2025-09-17 10:37:59 +0530

    Add files via upload

---
+++ Python-Practicals-1/reverse string.py
@@ -0,0 +1,11 @@
+s = "hello"
+reversed_s = s[::-1]
+print(reversed_s)  # Output: "olleh"
+
+reversed_s = ''.join(reversed(s))
+print(reversed_s)  # Output: "olleh"
+
+reversed_s = ''
+for char in s:
+    reversed_s = char + reversed_s
+print(reversed_s)  # Output: "olleh"
